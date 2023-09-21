b032b72bd1e8d15051ff554294ae8aa11f4403af
raulenzo <raru69@hotmail.com>
2023-09-21 15:17:25 +0200

    cambios

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import {router} from './portafolio/src/router/Index.jsx'
+import {router} from './router'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
